1d2908bef33c919c64297f5d9ff94c11e0fac4cb
Duy Tuan <ddt.tuando@gmail.com>
2022-03-11 17:25:20 +0700

    heroku

--- client/src/utils/FetchData.utils.js
+++ client/src/utils/FetchData.utils.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+// const baseURL = "http://localhost:5000";
 
 export const getAPI = async (url) => {
-  const response = await axios.get(`${baseURL}/api/${url}`);
+  const response = await axios.get(`/api/${url}`);
   return response;
 };
