ef1f2e38f472110326eff6b65b54e89cf923b8e3
Duy Tuan <ddt.tuando@gmail.com>
2022-03-11 17:16:29 +0700

    update deploy

--- server/index.js
+++ server/index.js
@@ -18,6 +18,14 @@ app.use("/api", routes.dictionaryRouter);
 // create Server
 const http = createServer(app);
 
+// Production Deploy
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 // server listening
 const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => {
